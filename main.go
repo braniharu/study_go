fd885cbc6efb0433c697f96e16349c0c1a8c09fc
braniharu <braniharu@gmail.com>
2022-10-09 19:24:51 +0300

    для среза Самолёт добавить значение через append

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	var airplane []Airplane = make([]Airplane, 2)
 	airplane[0] = Airplane{AirplaneName: "Boeing"}
 	airplane[1] = Airplane{AirplaneName: "British Aerospace"}
-	airplane = append(airplane, Airplane{AirplaneName: "\nFokker"})
+	airplane = append(airplane, Airplane{AirplaneName: "\nLancair"})
 
 	for i := range airplane {
 		go MakeFlight(airplane[i])
